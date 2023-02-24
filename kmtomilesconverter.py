b95ed2f6db2a077d643a1f1b67c7db844729e975
cypi735 <77378328+kipron00@users.noreply.github.com>
2023-02-24 06:09:15 +0300

    Add files via upload

---
+++ kmtomilesconverter.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 05:19:42 2023
+
+@author: cypri
+"""
+
+print("this program converts kilometers to miles\n")
+
+km = float(input("please enter the distance in kilometers: "))
+miles = km * 0.62137119
+
+print("\nthe distance is: ", km, "kilometers")
+
+print("\nthe distance in miles is: ", round(miles,3), "miles")
